da7cbc94470e4f684a5b3aafd02eceaed8862e62
tr3nb0lone <amanhaileg@gmail.com>
2025-01-14 20:25:41 +0000

    Initiated scan.sh

--- scan.sh
+++ scan.sh
@@ -2,6 +2,9 @@
 
 
 # nmap script wrapper to automatically scan targets:
+# Redirects rustscan's output to nmap:
+# todo:  implement a domain name extraction + automatic modification of the hosts file, implement support for udp scans
 
+rustscan -a $1 --ulimit 5000 -- -sCV -Pn -oN nmap
 
 
