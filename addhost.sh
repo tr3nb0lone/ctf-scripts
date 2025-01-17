fdcfe053c2e5aac3a683ade7b187908a153bfbac
tr3nb0lone <amanhaileg@gmail.com>
2025-01-17 13:57:11 +0000

    min update

---
+++ addhost.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+IP=$1
+HOST=$2
+
+
+function add(){
+
+    
+
+}
+
+
+
--- scan.sh
+++ scan.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 
-# nmap script wrapper to automatically scan targets:
-# Redirects rustscan's output to nmap:
+# script wrapper to automatically scan targets:
+
 # todo:  implement a domain name extraction + automatic modification of the hosts file, implement support for udp scans
 
 rustscan -a $1 --ulimit 5000 -- -sCV -Pn -oN nmap
