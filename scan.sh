d8d5e5a03a76d754b28636a5a65f5a0c6bf0a4b4
tr3nb0lone <amanhaileg@gmail.com>
2025-04-01 17:42:39 +0300

    Update readme.

--- scan.sh
+++ scan.sh
@@ -5,6 +5,5 @@
 
 # todo:  implement a domain name extraction + automatic modification of the hosts file, implement support for udp scans
 
-rustscan -a $1 --ulimit 5000 -- -sCV -Pn -oN nmap
-
+rustscan -a $1 --ulimit 5000 -- -sCV -T4 -Pn -oN nmap
 
