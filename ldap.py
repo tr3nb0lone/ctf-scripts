e9526591ab00658a625631fd12563e0892e738a5
tr3nb0lone <amanhaileg@gmail.com>
2025-05-05 23:42:55 +0300

    LDAP script initialised.

---
+++ ldap.py
@@ -0,0 +1,17 @@
+from ldap3 import *
+import sys
+
+IP = sys.argv[1]
+
+s = Server(IP, get_info=ALL)
+
+# anonymous bind
+c = Connection(s, '', '')
+
+if c.bind():
+    print("Anonymous bind successfull!")
+    print(s.info)
+else:
+    print("Anonymous  bind might not be successful!!")
+
+# gracefully exit.
